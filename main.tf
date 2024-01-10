0b5cd8c8d4d5506a1a35cf77176eede286fb045b
Rohith M <rohith.m068@gmail.com>
2024-01-10 11:30:16 -0600

    Added kubernetes cluster code

--- main.tf
+++ main.tf
@@ -1,18 +1,10 @@
 resource "aws_eks_cluster" "example" {
-  name     = "example"
+  name     = "${var.env}-eks"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = [aws_subnet.example1.id, aws_subnet.example2.id]
+    subnet_ids = var.subnet_ids
   }
 
 
-}
-
-output "endpoint" {
-  value = aws_eks_cluster.example.endpoint
-}
-
-output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.example.certificate_authority[0].data
 }
