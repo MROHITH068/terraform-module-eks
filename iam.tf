33b1f32d9fb1c1baf9becc9a585efe3358c7a35e
Rohith M <rohith.m068@gmail.com>
2024-01-10 11:25:21 -0600

    Added kubernetes cluster code

---
+++ iam.tf
@@ -0,0 +1,29 @@
+
+resource "aws_iam_role" "eks-role" {
+  name               = "eks-cluster-example"
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Action": "sts:AssumeRole",
+          "Principal": {
+            "Service": "eks.amazonaws.com"
+          },
+          "Effect": "Allow",
+          "Sid": ""
+        }
+      ]
+    })}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-role.name
+}
+
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks-role.name
+}
+
