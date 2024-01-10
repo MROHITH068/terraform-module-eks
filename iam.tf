0ea85aa4c7f72097d588a2265355df82da494515
Rohith M <rohith.m068@gmail.com>
2024-01-10 11:28:32 -0600

    Added kubernetes cluster code

--- iam.tf
+++ iam.tf
@@ -14,7 +14,8 @@ resource "aws_iam_role" "eks-role" {
           "Sid": ""
         }
       ]
-    })}
+    })
+}
 
 resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
