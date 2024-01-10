e57fad0ceec979e2ca5fe9ba5b499f648c8ea91b
Rohith M <rohith.m068@gmail.com>
2024-01-10 11:31:51 -0600

    Added kubernetes cluster code

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 resource "aws_eks_cluster" "example" {
   name     = "${var.env}-eks"
-  role_arn = aws_iam_role.example.arn
+  role_arn = aws_iam_role.eks-role.arn
 
   vpc_config {
     subnet_ids = var.subnet_ids
   }
-
-
 }
