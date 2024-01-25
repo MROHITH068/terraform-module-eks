6694195e7247b502701bb27b68ee6c1c775c2757
Rohith M <rohith.m068@gmail.com>
2024-01-24 20:53:41 -0600

    Created IAM role and policy for aws-ssm-parameters

--- iam.tf
+++ iam.tf
@@ -58,3 +58,60 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.node-role.name
 }
 
+
+resource "aws_iam_policy" "sa-policy" {
+  name        = "eks-${var.env}-ssm-pm-policy"
+  path        = "/"
+  description = "eks-${var.env}-ssm-pm-policy"
+
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter",
+            "ssm:DescribeParameters",
+            "kms:Decrypt"
+          ],
+          "Resource": concat(["arn:aws:ssm:us-east-1:403215663985:parameter/roboshop.*",
+            var.kms_key_arn])
+        }
+      ]
+    })
+}
+
+#Role
+resource "aws_iam_role" "sa-role" {
+  name = "eks-${var.env}-ssm-pm-role"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Principal": {
+            "Federated": aws_iam_openid_connect_provider.oidc-iam.arn
+          },
+          "Action": "sts:AssumeRoleWithWebIdentity",
+          "Condition": {
+            "StringEquals": {
+              "oidc.eks.region-code.amazonaws.com/id/${element(split("/", aws_iam_openid_connect_provider.oidc-iam.arn), 3)}:aud": "sts.amazonaws.com"
+            }
+          }
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "policy-attach" {
+  role      = aws_iam_role.sa-role.name
+  policy_arn = aws_iam_policy.sa-policy.arn
+}
--- variables.tf
+++ variables.tf
@@ -4,3 +4,4 @@ variable "min_size" {}
 variable "max_size" {}
 variable "capacity_type" {}
 variable "instance_type" {}
+variable "kms_key_arn" {}
